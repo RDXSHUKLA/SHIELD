ffa3dc2dd1e3cb464a6d3d9a5555fe9e4da78eab
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2024-11-16 02:33:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,18 +4,18 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID"))
+API_ID = int(os.environ.get("API_ID", "27838385"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH")
+API_HASH = os.environ.get("API_HASH", "0710bd2a89a41c3506f98f7e6fd7294a")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7268832007:AAHWMygjnhj-tMmLR2cYKyQQq98GZw0J9pE")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "GroupXshieldBot")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6762113050"))
 # ------------------X------------------------------
 # ------------------------------------------------
-LOGGER_ID = int(os.environ.get("LOGGER_ID"))
+LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1002018556839"))
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://SHASHANK:STRANGER@shashank.uj7lold.mongodb.net/?retryWrites=true&w=majority")
 # ------------------------------------------------
